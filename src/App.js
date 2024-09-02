a4b70161443cbdb04f93da8189ee0d9d04d37686
Hussain Syed <hussain.iqrar@heuristicsol.com>
2024-09-02 11:23:51 +0500

    Identity Creation Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AccountScreen from "./Components/Screens/Accounts";
+// import DynamicForm from "./Components/Screens/Particulars";
+import UserType from "./Components/Screens/UserType";
+import UserForm from "./Components/Screens/UserForm";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/accounts" element={<AccountScreen />} />
+        {/* <Route path="/accounts" element={<AccountScreen />} /> */}
+        {/* <Route path="/" element={<DynamicForm />} /> */}
+        {/* <Route path="/" element={<UserType />} /> */}
+        <Route path="/" element={<UserType />} />
+        <Route path="/user-form" element={<UserForm />} />
       </Routes>
     </BrowserRouter>
   );
