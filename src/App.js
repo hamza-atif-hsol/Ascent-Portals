aa9782d3f7e10184bcb967110673f3a3b0bef835
hamza-atif-hsol <hamza@heuristicsol.com>
2024-09-02 11:27:50 +0500

    Doc, FaceVerification, Vcip done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import './App.css';
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AccountScreen from "./Components/Screens/Accounts";
+import Documents from "./Components/Screens/Documents";
+import FaceVerifacation from "./Components/Screens/FaceVerification";
+import VCIP from "./Components/Screens/VCIP"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/accounts" element={<AccountScreen />} />
+        <Route path="/documents" element={<Documents />} />
+        <Route path="/faceverification" element={<FaceVerifacation/>} />
+        <Route path="/vcip" element={<VCIP/>} />
       </Routes>
     </BrowserRouter>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to bottom, #0d345d 0%, #151c39 100%)',
+      },
+    },
   },
   plugins: [],
 }
